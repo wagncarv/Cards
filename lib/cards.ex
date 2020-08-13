1c73e0913a50bf9ce94e57cb45ad9bcd25770f89
wagner carvalho <wagncarv@gmail.com>
2020-08-12 23:04:34 -0300

    changes

--- lib/cards.ex
+++ lib/cards.ex
@@ -28,8 +28,8 @@ defmodule Cards do
   def load(filename) do
 
     case File.read(filename) do
-      :ok -> :erlang.binary_to_term filename
-      :error -> "That file does not exist"
+      {:ok, binary} -> :erlang.binary_to_term binary
+      {:error, _reason} -> "That file does not exist"
       _ -> "Unknown error"
     end
   end
